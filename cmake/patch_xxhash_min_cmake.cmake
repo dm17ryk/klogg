9beb657e9beaad8fb3841dcd5fef9108b3692ba0
AgentK <dmitryk@3vium.com>
2026-01-07 22:55:34 +0200

    fix github action

--- cmake/patch_xxhash_min_cmake.cmake
+++ cmake/patch_xxhash_min_cmake.cmake
@@ -9,17 +9,17 @@ endif()
 
 file(READ "${patch_file}" content)
 string(REGEX MATCH "cmake_minimum_required\\(VERSION[ ]+([0-9]+(\\.[0-9]+)*)\\)" match "${content}")
-if(NOT match)
-  message(FATAL_ERROR "xxHash patch: no cmake_minimum_required in ${patch_file}")
-endif()
-
-set(found_version "${CMAKE_MATCH_1}")
-if(found_version VERSION_LESS "3.5")
-  string(
-    REGEX REPLACE "cmake_minimum_required\\(VERSION[ ]+[0-9.]+\\)"
-    "cmake_minimum_required(VERSION 3.5)" updated "${content}"
-  )
-  if(NOT updated STREQUAL content)
-    file(WRITE "${patch_file}" "${updated}")
+if(match)
+  set(found_version "${CMAKE_MATCH_1}")
+  if(found_version VERSION_LESS "3.5")
+    string(
+      REGEX REPLACE "cmake_minimum_required\\(VERSION[ ]+[0-9.]+\\)"
+      "cmake_minimum_required(VERSION 3.5)" updated "${content}"
+    )
+    if(NOT updated STREQUAL content)
+      file(WRITE "${patch_file}" "${updated}")
+    endif()
   endif()
+else()
+  file(WRITE "${patch_file}" "cmake_minimum_required(VERSION 3.5)\n\n${content}")
 endif()
