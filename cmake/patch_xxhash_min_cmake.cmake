56fcb53cc86926af5b0c0687db4c5cdab0c80432
AgentK <dmitryk@3vium.com>
2026-01-07 22:45:07 +0200

    fix github action

---
+++ cmake/patch_xxhash_min_cmake.cmake
@@ -0,0 +1,25 @@
+if(NOT DEFINED PATCH_ROOT)
+  set(PATCH_ROOT "${CMAKE_SOURCE_DIR}")
+endif()
+
+set(patch_file "${PATCH_ROOT}/cmake_unofficial/CMakeLists.txt")
+if(NOT EXISTS "${patch_file}")
+  message(FATAL_ERROR "xxHash patch: missing ${patch_file}")
+endif()
+
+file(READ "${patch_file}" content)
+string(REGEX MATCH "cmake_minimum_required\\(VERSION[ ]+([0-9]+(\\.[0-9]+)*)\\)" match "${content}")
+if(NOT match)
+  message(FATAL_ERROR "xxHash patch: no cmake_minimum_required in ${patch_file}")
+endif()
+
+set(found_version "${CMAKE_MATCH_1}")
+if(found_version VERSION_LESS "3.5")
+  string(
+    REGEX REPLACE "cmake_minimum_required\\(VERSION[ ]+[0-9.]+\\)"
+    "cmake_minimum_required(VERSION 3.5)" updated "${content}"
+  )
+  if(NOT updated STREQUAL content)
+    file(WRITE "${patch_file}" "${updated}")
+  endif()
+endif()
