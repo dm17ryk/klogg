491a4adc72080bccdd0bc330dc77a06686153c12
AgentK <dmitryk@3vium.com>
2026-01-12 18:38:06 +0200

    Add additional DLL and PDB files to release process

--- packaging/windows/prepare_release.cmd
+++ packaging/windows/prepare_release.cmd
@@ -47,6 +47,21 @@ xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_64_md_relwithdebinfo
 xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_relwithdebinfo\tbbmalloc_proxy.dll %KLOGG_WORKSPACE%\release\ /y
 xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_relwithdebinfo\tbbmalloc_proxy.pdb %KLOGG_WORKSPACE%\release\ /y
 
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_64_md_release\tbb12.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_64_md_release\tbb12.pdb %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_release\tbb12.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_release\tbb12.pdb %KLOGG_WORKSPACE%\release\ /y
+
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_64_md_release\tbbmalloc.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_64_md_release\tbbmalloc.pdb %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_release\tbbmalloc.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_release\tbbmalloc.pdb %KLOGG_WORKSPACE%\release\ /y
+
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_64_md_release\tbbmalloc_proxy.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_64_md_release\tbbmalloc_proxy.pdb %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_release\tbbmalloc_proxy.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\msvc_19.44_cxx17_32_md_release\tbbmalloc_proxy.pdb %KLOGG_WORKSPACE%\release\ /y
+
 xcopy %KLOGG_WORKSPACE%\%KLOGG_BUILD_ROOT%\generated\documentation.html %KLOGG_WORKSPACE%\release\ /y
 xcopy %KLOGG_WORKSPACE%\COPYING %KLOGG_WORKSPACE%\release\ /y
 xcopy %KLOGG_WORKSPACE%\NOTICE %KLOGG_WORKSPACE%\release\ /y
