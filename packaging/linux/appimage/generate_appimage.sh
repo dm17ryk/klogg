698be236f434b6d87e3031ffa22a8625d7bd213a
AgentK <dmitryk@3vium.com>
2026-03-07 02:51:32 +0200

    ci: add arm64 targets and unify continuous release artifacts

--- packaging/linux/appimage/generate_appimage.sh
+++ packaging/linux/appimage/generate_appimage.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+set -e
+
+ARCH="$(uname -m)"
+LINUXDEPLOYQT_ARCH="x86_64"
+LIB_DIR="/lib/x86_64-linux-gnu"
+
+if [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+  LINUXDEPLOYQT_ARCH="aarch64"
+  LIB_DIR="/lib/aarch64-linux-gnu"
+fi
 
 DESTDIR=$(readlink -f appdir) ninja install
-wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-${LINUXDEPLOYQT_ARCH}.AppImage"
+chmod a+x "linuxdeployqt-continuous-${LINUXDEPLOYQT_ARCH}.AppImage"
 
-VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
+VERSION=$KLOGG_VERSION "./linuxdeployqt-continuous-${LINUXDEPLOYQT_ARCH}.AppImage" appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
 
 mkdir -p appdir/usr/lib
-cp /lib/x86_64-linux-gnu/libssl* appdir/usr/lib
+cp "${LIB_DIR}"/libssl* appdir/usr/lib
 
-VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+VERSION=$KLOGG_VERSION "./linuxdeployqt-continuous-${LINUXDEPLOYQT_ARCH}.AppImage" appdir/usr/share/applications/*.desktop -appimage
 
-mkdir ./packages
-cp ./klogg-$KLOGG_VERSION-x86_64.AppImage ./packages/klogg-$KLOGG_VERSION-x86_64.AppImage
+mkdir -p ./packages
+APPIMAGE_FILE="$(ls ./klogg-$KLOGG_VERSION-*.AppImage | head -n 1)"
+cp "${APPIMAGE_FILE}" "./packages/$(basename "${APPIMAGE_FILE}")"
