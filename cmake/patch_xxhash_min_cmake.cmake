1226391b35da10eeab6f7f76cafcd504609ceb89
AgentK <dmitryk@3vium.com>
2026-03-09 22:21:05 +0200

    \ build: patch xxhash Windows ARM64 neon include\

--- cmake/patch_xxhash_min_cmake.cmake
+++ cmake/patch_xxhash_min_cmake.cmake
@@ -15,3 +15,27 @@ string(
   "${content}"
 )
 file(WRITE "${patch_file}" "cmake_minimum_required(VERSION 3.10)\n\n${stripped}")
+
+set(xxhash_header "${PATCH_ROOT}/xxhash.h")
+if(EXISTS "${xxhash_header}")
+  file(READ "${xxhash_header}" header_content)
+  set(header_updated "${header_content}")
+
+  string(REPLACE [=[#elif defined(_MSC_VER)
+#  include <intrin.h>
+#endif
+]=]
+                 [=[#elif defined(_MSC_VER)
+#  if defined(_M_ARM64) || defined(_M_ARM64EC)
+#    include <arm64_neon.h>
+#  endif
+#  include <intrin.h>
+#endif
+]=]
+                 header_updated
+                 "${header_updated}")
+
+  if(NOT header_updated STREQUAL header_content)
+    file(WRITE "${xxhash_header}" "${header_updated}")
+  endif()
+endif()
